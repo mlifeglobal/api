6951689816229889542a1fadbed81b5379f3c036
nbaghiro <naib.baghirov@gmail.com>
2018-07-27 21:35:09 -0400

    survey migration

---
+++ models/survey/migrations/1.js
@@ -0,0 +1,63 @@
+module.exports = Sequelize => ({
+  up (queryInterface) {
+    return queryInterface
+      .createTable('surveys', {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+          field: 'id'
+        },
+        name: {
+          type: Sequelize.STRING,
+          field: 'name'
+        },
+        description: {
+          type: Sequelize.STRING,
+          field: 'description'
+        },
+        incentive: {
+          type: Sequelize.INTEGER,
+          field: 'incentive'
+        },
+        state: {
+          type: Sequelize.ENUM,
+          values: ['uninitiated', 'in_progress', 'completed'],
+          defaultValue: 'uninitiated',
+          field: 'state'
+        },
+        completedCount: {
+          type: Sequelize.INTEGER,
+          defaultValue: 0,
+          field: 'completed_count'
+        },
+        optInCodes: {
+          type: Sequelize.ARRAY(Sequelize.STRING),
+          defaultValue: [],
+          field: 'opt_in_codes'
+        },
+        platforms: {
+          type: Sequelize.ARRAY(Sequelize.STRING),
+          defaultValue: [],
+          field: 'platforms'
+        },
+        createdDate: {
+          type: Sequelize.DATE,
+          field: 'created_date'
+        },
+        publishedDate: {
+          type: Sequelize.DATE,
+          field: 'published_date'
+        },
+        closedDate: {
+          type: Sequelize.DATE,
+          field: 'closed_date'
+        }
+      })
+  },
+  down (queryInterface) {
+    return queryInterface
+      .dropTable('surveys')
+  }
+})
---
+++ models/survey/model.js
@@ -0,0 +1,58 @@
+module.exports = (bcrypt, config, JWT, mail, moment, Sequelize, twilio, uuid, amplitude, aws) => ({
+  attributes: {
+    name: {
+      type: Sequelize.STRING,
+      field: 'name'
+    },
+    description: {
+      type: Sequelize.STRING,
+      field: 'description'
+    },
+    incentive: {
+      type: Sequelize.INTEGER,
+      field: 'incentive'
+    },
+    state: {
+      type: Sequelize.ENUM,
+      values: ['uninitiated', 'in_progress', 'completed'],
+      defaultValue: 'uninitiated',
+      field: 'state'
+    },
+    completedCount: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+      field: 'completed_count'
+    },
+    optInCodes: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      defaultValue: [],
+      field: 'opt_in_codes'
+    },
+    platforms: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      defaultValue: [],
+      field: 'platforms'
+    },
+    createdDate: {
+      type: Sequelize.DATE,
+      field: 'created_date'
+    },
+    publishedDate: {
+      type: Sequelize.DATE,
+      field: 'published_date'
+    },
+    closedDate: {
+      type: Sequelize.DATE,
+      field: 'closed_date'
+    }
+  },
+  classMethods: {},
+  instanceMethods: {},
+  associations: {},
+  hooks: {},
+  indexes: [],
+  timestamps: true,
+  createdAt: 'createdDate',
+  publishedDate: false,
+  closedDate: false
+})
